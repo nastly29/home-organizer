7c791fb19bab1c648f41141c0c2b858581a3e7b3
nastly29 <anastasiadavid312@gmail.com>
2026-01-05 22:35:53 +0200

    change index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
+//const path = require("path");
 
 const usersRouter = require("./routes/users");
 const teamsRouter = require("./routes/teams");
@@ -12,11 +12,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
 
-const isProd = process.env.NODE_ENV === "production";
+app.use(cors({
+  origin: CLIENT_ORIGIN,
+  credentials:true
+}))
+//const isProd = process.env.NODE_ENV === "production";
 
-if (!isProd) {
-  app.use(cors({ origin: CLIENT_ORIGIN, credentials: true }));
-}
+//if (!isProd) {
+  //app.use(cors({ origin: CLIENT_ORIGIN, credentials: true }));
+//}
 
 app.use(express.json());
 
@@ -27,18 +31,18 @@ app.use("/api/teams", teamsRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/teams", teamDashboard);
 
-if (isProd) {
-  const clientBuildPath = path.join(__dirname, "..", "client", "build");
-  app.use(express.static(clientBuildPath));
-  app.get(/^(?!\/api).*/, (req, res) => {
-    res.sendFile(path.join(clientBuildPath, "index.html"));
-  });
-}
+//if (isProd) {
+  //const clientBuildPath = path.join(__dirname, "..", "client", "build");
+  //app.use(express.static(clientBuildPath));
+  //app.get(/^(?!\/api).*/, (req, res) => {
+    //res.sendFile(path.join(clientBuildPath, "index.html"));
+  //});
+//} 
 
-app.use((err, req, res, next) => {
+/*app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ error: "Internal server error" });
-});
+});*/
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
